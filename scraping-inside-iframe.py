41f7e24f0143aefe897d942a0361361d7029deac
Bee <beekurt98@gmail.com>
2024-10-05 02:35:33 +0300

    Update scraping-inside-iframe.py

--- scraping-inside-iframe.py
+++ scraping-inside-iframe.py
@@ -2,20 +2,28 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-URL = "https://beekurt98.github.io/iframe-tutorial-selenium/"
+# Get the URL of the desired website that contains the iframe
+URL = "https://beekurt98.github.io/iframe-tutorial-selenium/"  
 
 driver = webdriver.Chrome()
 
-driver.get(URL)
+driver.get(URL) 
+
+# Can be adjusted depending on the overall internet speed
 time.sleep(3)
 
-iframe = driver.find_element(By.XPATH, value='/html/body/div/iframe')
+# Selects the iframe element
+iframe = driver.find_element(By.XPATH, value='/html/body/div/iframe')  
+
+# Switches the iframe into a frame element
 driver.switch_to.frame(iframe)
 
+# These are the elements inside of the iframe-turned-into-frame
 h1 = driver.find_element(By.XPATH, '/html/body/div/h1')
 paragraph = driver.find_element(By.XPATH, '/html/body/div/p[1]')
 a_link = driver.find_element(By.XPATH, '/html/body/div/p[2]/a')
 
+# Will print the results. 
 print(h1.text)
 print(paragraph.text)
 print(a_link.get_attribute("href"))
